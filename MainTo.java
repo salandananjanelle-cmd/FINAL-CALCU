d0a39269d4bc69d90b1b50be6a703c64557671cb
Janelle <salandananjanelle@gmail.cpm>
2026-01-30 11:00:02 +0800

    addition

---
+++ MainTo.java
@@ -0,0 +1,22 @@
+package addition;
+
+import java.util.Scanner;
+
+public class Addition {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
